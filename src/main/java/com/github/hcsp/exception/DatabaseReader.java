67088c5c9e4ded1ad677c45d1c720b5ef5f91be4
zorofun <choupeisi@gmail.com>
2020-03-06 17:24:01 +0800

    resultSet关闭

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -8,29 +8,22 @@ public class DatabaseReader {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
-        ResultSet resultSet = null;
         try (Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "22");
              PreparedStatement statement = connection.prepareStatement("select * from PULL_REQUESTS where number > ?")) {
             statement.setInt(1, 0);
-            resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                System.out.println(
-                        resultSet.getInt(1)
-                                + " "
-                                + resultSet.getString(2)
-                                + " "
-                                + resultSet.getString(2));
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    System.out.println(
+                            resultSet.getInt(1)
+                                    + " "
+                                    + resultSet.getString(2)
+                                    + " "
+                                    + resultSet.getString(2));
+                }
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
 
     }
