e22dd309c784e7cdff0b1235aa51f7e240c08fd2
georgeT <jobsteng@163.com>
2020-12-08 16:18:34 +0800

    IO与异常实战：正确处理数据库连接的异常

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -4,15 +4,13 @@ import java.io.File;
 import java.sql.*;
 
 public class DatabaseReader {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(jdbcUrl, "sa", "");
-            PreparedStatement statement =
-                    connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
+
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "")) {
+            PreparedStatement statement = connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
             statement.setInt(1, 0);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
@@ -23,10 +21,8 @@ public class DatabaseReader {
                                 + " "
                                 + resultSet.getString(2));
             }
-        } finally {
-            assert connection != null;
-            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
     }
 }
