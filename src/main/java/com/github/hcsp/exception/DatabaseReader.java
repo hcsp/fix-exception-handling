4f7b3d0d0521ef6d1d3843537416cdec6a64118a
3stonelei <1462070965@qq.com>
2022-06-07 20:51:55 +0800

    异常1

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -4,15 +4,12 @@ import java.io.File;
 import java.sql.*;
 
 public class DatabaseReader {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
 
-        try {
-            Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "");
-            PreparedStatement statement =
-                    connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, "sa", ""); PreparedStatement statement = connection.prepareStatement("select * from PULL_REQUESTS where number > ?")) {
             statement.setInt(1, 0);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
